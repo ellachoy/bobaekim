55f70fb8dbd38d021d0850ddad17498d4a89c332
ella choy <egal21de@gmail.com>
2024-05-06 01:12:13 +0200

    setup projekt

---
+++ src/page/main.jsx
@@ -0,0 +1,14 @@
+import './styles/main.scss'
+import TypeIt from 'typeit-react'
+
+export default function Main() {
+  return (
+    <div className='section wrapper'>
+      <h1 className='h1 logo'>
+        <TypeIt>
+          <span className='logo-first'>BOBAE</span> KIM
+        </TypeIt>
+      </h1>
+    </div>
+  )
+}
