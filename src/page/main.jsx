d88f8a5d7f8bb0f194911d8d1e2123797885256f
ella choy <egal21de@gmail.com>
2024-05-06 01:31:18 +0200

    Passt class-name an

--- src/page/main.jsx
+++ src/page/main.jsx
@@ -4,9 +4,9 @@ import TypeIt from 'typeit-react'
 export default function Main() {
   return (
     <div className='section wrapper'>
-      <h1 className='h1 logo'>
+      <h1 className='h1 intro'>
         <TypeIt>
-          <span className='logo-first'>BOBAE</span> KIM
+          <span className='intro-first'>BOBAE</span> KIM
         </TypeIt>
       </h1>
     </div>
