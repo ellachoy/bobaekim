f41bb9b35e8ac674a3ff82a5c14caaccce65ced0
ella choy <egal21de@gmail.com>
2024-05-08 18:34:02 +0200

    Erzeugt die Ausgabe des Introduce-pages

--- src/pages/Introduce.jsx
+++ src/pages/Introduce.jsx
@@ -1,5 +1,39 @@
 import React from 'react'
+import foto1 from '../assets/5336.jpg'
+import Navigation from './components/Navigation'
 
 export default function Introduce() {
-  return <div>About Me</div>
+  return (
+    <>
+      <section className='section  wrapper'>
+        <div className='introduce'>
+          <div className='intro-image'>
+            <img src={foto1} alt='' />
+          </div>
+          <div>
+            <h2 className='h3'>Hello, iam Bobae Kim </h2>
+            <blockquote>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat,
+            </blockquote>
+            <div style={{ 'margin-top': '88px' }}>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+              dolor sit amet.
+            </div>
+          </div>
+        </div>
+
+        <Navigation />
+      </section>
+    </>
+  )
 }
--- src/pages/components/Navigation.jsx
+++ src/pages/components/Navigation.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navigation({ active }) {
-  
   return (
     <>
       <nav className='wrapper show-nav'>
         <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
           <li>
             <Link to='/Introduce'>Introduce</Link>
           </li>
