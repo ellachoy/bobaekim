27cc0e51bfd99478a3085c5bca1a439cbf778761
ella choy <egal21de@gmail.com>
2024-05-08 19:52:43 +0200

    Erzeugt die Ausgabe des Works-pages

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,5 +1,61 @@
 import React from 'react'
+import img1 from '../assets/works2/IMG_0001.jpg'
+import img3 from '../assets/works2/IMG_0003.jpg'
+import img4 from '../assets/works2/IMG_0004.jpg'
+import img5 from '../assets/works2/IMG_0005.jpg'
+import img6 from '../assets/works2/IMG_0006.jpg'
+import img7 from '../assets/works2/IMG_0007.jpg'
+import img11 from '../assets/works2/IMG_0011.jpg'
+import img12 from '../assets/works2/IMG_0012.jpg'
+import img13 from '../assets/works2/IMG_0013.jpg'
+import img16 from '../assets/works2/IMG_0016.jpg'
+import img17 from '../assets/works2/IMG_0017.jpg'
+import img19 from '../assets/works2/IMG_0019.jpg'
+import img20 from '../assets/works1/IMG_0147.jpeg'
+import img21 from '../assets/works1/IMG_0917.jpeg'
+import img22 from '../assets/works1/IMG_2852.jpeg'
+import img23 from '../assets/works1/IMG_5525.jpeg'
+import img24 from '../assets/works1/IMG_5870.jpeg'
+import img25 from '../assets/works1/IMG_6315.jpeg'
+import img26 from '../assets/works1/IMG_6316.jpeg'
+import img27 from '../assets/works1/IMG_8021.jpeg'
+import img28 from '../assets/works1/IMG_8836.jpeg'
+import img29 from '../assets/works1/IMG_9424.jpeg'
+import img30 from '../assets/works1/IMG_9692.jpeg'
+import img31 from '../assets/works1/IMG_9728 (1).jpeg'
+import Navigation from './components/Navigation'
 
 export default function Works() {
-  return <div>Works</div>
+  return (
+    <section className='section wrapper'>
+      <div className='works gallery'>
+        <img src={img1} alt='' />
+        <img src={img3} alt='' />
+        <img src={img4} alt='' />
+        <img src={img5} alt='' />
+        <img src={img6} alt='' />
+        <img src={img7} alt='' />
+        <img src={img11} alt='' />
+        <img src={img12} alt='' />
+        <img src={img13} alt='' />
+
+        <img src={img16} alt='' />
+        <img src={img17} alt='' />
+        <img src={img19} alt='' />
+        <img src={img20} alt='' />
+        <img src={img21} alt='' />
+        <img src={img22} alt='' />
+        <img src={img23} alt='' />
+        <img src={img24} alt='' />
+        <img src={img25} alt='' />
+        <img src={img26} alt='' />
+        <img src={img27} alt='' />
+        <img src={img28} alt='' />
+        <img src={img29} alt='' />
+        <img src={img30} alt='' />
+        <img src={img31} alt='' />
+      </div>
+      <Navigation />
+    </section>
+  )
 }
