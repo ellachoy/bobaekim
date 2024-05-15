20c27ef1bdcfac585839ea3ea1bf4f263b5e9fa2
ella choy <egal21de@gmail.com>
2024-05-15 14:56:36 +0200

    Lagert Images um

---
+++ src/pages/data/images.jsx
@@ -0,0 +1,57 @@
+import img1 from '../../assets/works2/IMG_0001.jpg'
+import img2 from '../../assets/works2/IMG_0003.jpg'
+import img3 from '../../assets/works2/IMG_0004.jpg'
+import img4 from '../../assets/works2/IMG_0005.jpg'
+import img5 from '../../assets/works2/IMG_0006.jpg'
+import img6 from '../../assets/works2/IMG_0007.jpg'
+import img7 from '../../assets/works2/IMG_0011.jpg'
+import img8 from '../../assets/works2/IMG_0012.jpg'
+import img9 from '../../assets/works2/IMG_0013.jpg'
+import img10 from '../../assets/works2/IMG_0016.jpg'
+import img11 from '../../assets/works2/IMG_0017.jpg'
+import img12 from '../../assets/works2/IMG_0019.jpg'
+import img25 from '../../assets/works2/IMG_0025.jpeg'
+import img26 from '../../assets/works2/IMG_0026.jpeg'
+import img13 from '../../assets/works1/IMG_0147.jpeg'
+import img14 from '../../assets/works1/IMG_0917.jpeg'
+import img15 from '../../assets/works1/IMG_2852.jpeg'
+import img16 from '../../assets/works1/IMG_5525.jpeg'
+import img17 from '../../assets/works1/IMG_5870.jpeg'
+import img18 from '../../assets/works1/IMG_6315.jpeg'
+import img19 from '../../assets/works1/IMG_6316.jpeg'
+import img20 from '../../assets/works1/IMG_8021.jpeg'
+import img21 from '../../assets/works1/IMG_8836.jpeg'
+import img22 from '../../assets/works1/IMG_9424.jpeg'
+import img23 from '../../assets/works1/IMG_9692.jpeg'
+import img24 from '../../assets/works1/IMG_9728 (1).jpeg'
+
+const Images = [
+  { id: 1, url: img1 },
+  { id: 2, url: img2 },
+  { id: 3, url: img3 },
+  { id: 4, url: img4 },
+  { id: 5, url: img5 },
+  { id: 6, url: img6 },
+  { id: 7, url: img7 },
+  { id: 8, url: img8 },
+  { id: 9, url: img9 },
+  { id: 10, url: img10 },
+  { id: 11, url: img11 },
+  { id: 12, url: img12 },
+  { id: 13, url: img26 },
+  { id: 14, url: img25 },
+  { id: 15, url: img13 },
+  { id: 16, url: img14 },
+  { id: 17, url: img15 },
+  { id: 18, url: img16 },
+  { id: 19, url: img17 },
+  { id: 20, url: img18 },
+  { id: 21, url: img19 },
+  { id: 22, url: img20 },
+  { id: 23, url: img21 },
+  { id: 24, url: img22 },
+  { id: 25, url: img23 },
+  { id: 26, url: img24 },
+]
+
+export default Images
