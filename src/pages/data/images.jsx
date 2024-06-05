176999fc2da23ca40d4db1d0a80b5944ad9201c1
ella choy <egal21de@gmail.com>
2024-06-05 19:50:38 +0200

    Entfernt unnötige imports

--- src/pages/data/images.jsx
+++ src/pages/data/images.jsx
@@ -1,29 +1,5 @@
-import img1 from '../../assets/works2/IMG_0001.jpg'
-import img2 from '../../assets/works2/IMG_0003.jpg'
-import img3 from '../../assets/works2/IMG_0004.jpg'
-import img4 from '../../assets/works2/IMG_0005.jpg'
-import img5 from '../../assets/works2/IMG_0006.jpg'
-import img6 from '../../assets/works2/IMG_0007.jpg'
-import img7 from '../../assets/works2/IMG_0011.jpg'
-import img8 from '../../assets/works2/IMG_0012.jpg'
-import img9 from '../../assets/works2/IMG_0013.jpg'
-import img10 from '../../assets/works2/IMG_0016.jpg'
-import img11 from '../../assets/works2/IMG_0017.jpg'
-import img12 from '../../assets/works2/IMG_0019.jpg'
-import img25 from '../../assets/works2/IMG_0025.jpeg'
-import img26 from '../../assets/works2/IMG_0026.jpeg'
-import img13 from '../../assets/works1/IMG_0147.jpeg'
-import img14 from '../../assets/works1/IMG_0917.jpeg'
-import img15 from '../../assets/works1/IMG_2852.jpeg'
-import img16 from '../../assets/works1/IMG_5525.jpeg'
-import img17 from '../../assets/works1/IMG_5870.jpeg'
 import img18 from '../../assets/works1/IMG_6315.jpeg'
 import img19 from '../../assets/works1/IMG_6316.jpeg'
-import img20 from '../../assets/works1/IMG_8021.jpeg'
-import img21 from '../../assets/works1/IMG_8836.jpeg'
-import img22 from '../../assets/works1/IMG_9424.jpeg'
-import img23 from '../../assets/works1/IMG_9692.jpeg'
-import img24 from '../../assets/works1/IMG_9728 (1).jpeg'
 import img27 from '../../assets/untitled/05.2024 People looking at a convex Mirror.jpg'
 import img28 from '../../assets/untitled/05.2024 Person seen by an Ant.jpg'
 import img29 from '../../assets/untitled/06.2020 Palm.jpg'
@@ -39,10 +15,8 @@ import img38 from '../../assets/untitled/2020 Unknown.jpg'
 import img39 from '../../assets/untitled/Missed Mama but can´t see.jpg'
 import img40 from '../../assets/reflectionOcean/2022 in the Netherlands (1).jpg'
 import img41 from '../../assets/friend/2022 Didactika (1).jpg'
-import img42 from '../../assets/glasses/2024 Wine glass series 1-1.jpg'
 import img43 from '../../assets/glasses/2024 Wine glass series 1-2.jpg'
 import img44 from '../../assets/glasses/2024 Wine glass series 2.jpg'
-
 import img45 from '../../assets/muse/02.11.2017 Guten Morgen.jpg'
 import img46 from '../../assets/muse/04.2024 Anna holding Flowers.jpg'
 import img47 from '../../assets/muse/05.2024 Rest.jpg'
@@ -70,32 +44,8 @@ import img68 from '../../assets/muse/sometimes Anna.jpg'
 import img69 from '../../assets/muse/Violette Anna.jpg'
 
 const Images = [
-  // { id: 1, category: 'My Muse', url: img1 },
-  // { id: 2, category: 'My Muse', url: img2 },
-  // { id: 3, category: 'My Muse', url: img3 },
-  // { id: 4, category: 'My Muse', url: img4 },
-  // { id: 6, category: 'My Muse', url: img6 },
-  // { id: 7, category: 'Reflection and Ocean', url: img7 },
-  // { id: 5, category: 'Untitled', url: img5 },
-  // { id: 8, category: 'Untitled', url: img8 },
-  // { id: 9, category: 'Untitled', url: img9 },
-  // { id: 16, category: 'Untitled', url: img14 },
-  // { id: 18, category: 'Untitled', url: img16 },
-  // { id: 10, category: 'My Muse', url: img10 },
-  // { id: 11, category: 'My Muse', url: img11 },
-  // { id: 12, category: 'My Muse', url: img12 },
-  // { id: 13, category: 'My Muse', url: img26 },
-  // { id: 14, category: 'My Muse', url: img25 },
-  // { id: 15, category: 'Reflection and Ocean', url: img13 },
-  // { id: 17, category: 'For Friends', url: img15 },
-  // { id: 19, category: 'Reflection and Ocean', url: img17 },
   { id: 20, category: 'Reflection and Ocean', url: img18 },
   { id: 21, category: 'Reflection and Ocean', url: img19 },
-  // { id: 22, category: 'Untitled', url: img20 },
-  // { id: 23, category: 'Glasses', url: img21 },
-  // { id: 24, category: 'For Friends', url: img22 },
-  // { id: 25, category: 'For Friends', url: img23 },
-  // { id: 26, category: 'Glasses', url: img24 },
   { id: 27, category: 'Untitled', url: img27 },
   { id: 28, category: 'Untitled', url: img28 },
   { id: 29, category: 'Untitled', url: img29 },
@@ -111,7 +61,6 @@ const Images = [
   { id: 39, category: 'Untitled', url: img39 },
   { id: 40, category: 'Reflection and Ocean', url: img40 },
   { id: 41, category: 'For Friends', url: img41 },
-  // { id: 42, category: 'Glasses', url: img42 },
   { id: 43, category: 'Glasses', url: img43 },
   { id: 44, category: 'Glasses', url: img44 },
   { id: 45, category: 'My Muse', url: img45 },
