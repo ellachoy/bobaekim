1edb5149d163cbbad981e4b99c21c37f989f0915
ella choy <egal21de@gmail.com>
2024-06-05 16:51:47 +0200

    update bildergalerie

--- src/pages/components/ImageList.jsx
+++ src/pages/components/ImageList.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import Fancybox from './Fancybox'
 
 export default function ImageList({ item }) {
   return (
     <div className='gallery'>
-      {item.map(({ id, url, category }) => (
-        <div key={id}>
-          <img src={url} alt={category} />
-        </div>
-      ))}
+      <Fancybox
+        options={{
+          Carousel: {
+            infinite: false,
+          },
+        }}
+      >
+        {item.map(({ id, url, category }) => (
+          <a key={id} href={url} data-fancybox='gallery'>
+            <img src={url} alt={category} />
+          </a>
+        ))}
+      </Fancybox>
     </div>
   )
 }
--- src/pages/data/images.jsx
+++ src/pages/data/images.jsx
@@ -24,34 +24,121 @@ import img21 from '../../assets/works1/IMG_8836.jpeg'
 import img22 from '../../assets/works1/IMG_9424.jpeg'
 import img23 from '../../assets/works1/IMG_9692.jpeg'
 import img24 from '../../assets/works1/IMG_9728 (1).jpeg'
+import img27 from '../../assets/untitled/05.2024 People looking at a convex Mirror.jpg'
+import img28 from '../../assets/untitled/05.2024 Person seen by an Ant.jpg'
+import img29 from '../../assets/untitled/06.2020 Palm.jpg'
+import img30 from '../../assets/untitled/07.03.2018 Laune .jpg'
+import img31 from '../../assets/untitled/09.07.2017 Kitchen.jpg'
+import img32 from '../../assets/untitled/2017 Annette in Bonn.jpg'
+import img33 from '../../assets/untitled/2017 In the Kitchen.jpg'
+import img34 from '../../assets/untitled/2017 Water bottle.jpg'
+import img35 from '../../assets/untitled/2019 I love David Hockney.jpg'
+import img36 from '../../assets/untitled/217 Finale Alle Brave - Son Yeol Eum.jpg'
+import img37 from '../../assets/untitled/Flowers on the refrigerator.jpg'
+import img38 from '../../assets/untitled/2020 Unknown.jpg'
+import img39 from '../../assets/untitled/Missed Mama but can´t see.jpg'
+import img40 from '../../assets/reflectionOcean/2022 in the Netherlands (1).jpg'
+import img41 from '../../assets/friend/2022 Didactika (1).jpg'
+import img42 from '../../assets/glasses/2024 Wine glass series 1-1.jpg'
+import img43 from '../../assets/glasses/2024 Wine glass series 1-2.jpg'
+import img44 from '../../assets/glasses/2024 Wine glass series 2.jpg'
+
+import img45 from '../../assets/muse/02.11.2017 Guten Morgen.jpg'
+import img46 from '../../assets/muse/04.2024 Anna holding Flowers.jpg'
+import img47 from '../../assets/muse/05.2024 Rest.jpg'
+import img48 from '../../assets/muse/06.03.2018 Laune.jpg'
+import img49 from '../../assets/muse/06.2017 Body auf dem Bett.jpg'
+import img50 from '../../assets/muse/06.2017 Inside the Baby carrier.jpg'
+import img51 from '../../assets/muse/06.2018 Anna.jpg'
+import img52 from '../../assets/muse/09.06.2017 Shadow.jpg'
+import img53 from '../../assets/muse/20.09.2017 Badezimmer.jpg'
+import img54 from '../../assets/muse/20.09.2017 Incheon Airport.jpg'
+import img55 from '../../assets/muse/20.09.2017 Papa vs Anna.jpg'
+import img56 from '../../assets/muse/20.11.2017 Waxfarbe.jpg'
+import img57 from '../../assets/muse/22.05.2017 umdrehen ing.jpg'
+import img58 from '../../assets/muse/23.02.2018 Laune.jpg'
+import img59 from '../../assets/muse/24.10.2017 Viollette Anna.jpg'
+import img60 from '../../assets/muse/25.06.2017 rest.jpg'
+import img61 from '../../assets/muse/25.06.2017 squeeze.jpg'
+import img62 from '../../assets/muse/25.07.2017 Umdrehen.jpg'
+import img63 from '../../assets/muse/2017 Anna.jpg'
+import img64 from '../../assets/muse/2017 Pajamas like Ice cream.jpg'
+import img65 from '../../assets/muse/2017 She looks at the camera.jpg'
+import img66 from '../../assets/muse/2018 outgrown the Baby carrier.jpg'
+import img67 from '../../assets/muse/2018 Sommer.jpg'
+import img68 from '../../assets/muse/sometimes Anna.jpg'
+import img69 from '../../assets/muse/Violette Anna.jpg'
 
 const Images = [
-  { id: 1, category: 'My Muse', url: img1 },
-  { id: 2, category: 'My Muse', url: img2 },
-  { id: 3, category: 'My Muse', url: img3 },
-  { id: 4, category: 'My Muse', url: img4 },
-  { id: 5, category: 'Untitled', url: img5 },
-  { id: 6, category: 'My Muse', url: img6 },
-  { id: 7, category: 'Reflection and Ocean', url: img7 },
-  { id: 8, category: 'Untitled', url: img8 },
-  { id: 9, category: 'Untitled', url: img9 },
-  { id: 10, category: 'My Muse', url: img10 },
-  { id: 11, category: 'My Muse', url: img11 },
-  { id: 12, category: 'My Muse', url: img12 },
-  { id: 13, category: 'My Muse', url: img26 },
-  { id: 14, category: 'My Muse', url: img25 },
-  { id: 15, category: 'Reflection and Ocean', url: img13 },
-  { id: 16, category: 'Untitled', url: img14 },
-  { id: 17, category: 'For Friends', url: img15 },
-  { id: 18, category: 'Untitled', url: img16 },
-  { id: 19, category: 'Reflection and Ocean', url: img17 },
+  // { id: 1, category: 'My Muse', url: img1 },
+  // { id: 2, category: 'My Muse', url: img2 },
+  // { id: 3, category: 'My Muse', url: img3 },
+  // { id: 4, category: 'My Muse', url: img4 },
+  // { id: 6, category: 'My Muse', url: img6 },
+  // { id: 7, category: 'Reflection and Ocean', url: img7 },
+  // { id: 5, category: 'Untitled', url: img5 },
+  // { id: 8, category: 'Untitled', url: img8 },
+  // { id: 9, category: 'Untitled', url: img9 },
+  // { id: 16, category: 'Untitled', url: img14 },
+  // { id: 18, category: 'Untitled', url: img16 },
+  // { id: 10, category: 'My Muse', url: img10 },
+  // { id: 11, category: 'My Muse', url: img11 },
+  // { id: 12, category: 'My Muse', url: img12 },
+  // { id: 13, category: 'My Muse', url: img26 },
+  // { id: 14, category: 'My Muse', url: img25 },
+  // { id: 15, category: 'Reflection and Ocean', url: img13 },
+  // { id: 17, category: 'For Friends', url: img15 },
+  // { id: 19, category: 'Reflection and Ocean', url: img17 },
   { id: 20, category: 'Reflection and Ocean', url: img18 },
   { id: 21, category: 'Reflection and Ocean', url: img19 },
-  { id: 22, category: 'Untitled', url: img20 },
-  { id: 23, category: 'Glasses', url: img21 },
-  { id: 24, category: 'For Friends', url: img22 },
-  { id: 25, category: 'For Friends', url: img23 },
-  { id: 26, category: 'Glasses', url: img24 },
+  // { id: 22, category: 'Untitled', url: img20 },
+  // { id: 23, category: 'Glasses', url: img21 },
+  // { id: 24, category: 'For Friends', url: img22 },
+  // { id: 25, category: 'For Friends', url: img23 },
+  // { id: 26, category: 'Glasses', url: img24 },
+  { id: 27, category: 'Untitled', url: img27 },
+  { id: 28, category: 'Untitled', url: img28 },
+  { id: 29, category: 'Untitled', url: img29 },
+  { id: 30, category: 'Untitled', url: img30 },
+  { id: 31, category: 'Untitled', url: img31 },
+  { id: 32, category: 'Untitled', url: img32 },
+  { id: 33, category: 'Untitled', url: img33 },
+  { id: 34, category: 'Untitled', url: img34 },
+  { id: 35, category: 'Untitled', url: img35 },
+  { id: 36, category: 'Untitled', url: img36 },
+  { id: 37, category: 'Untitled', url: img37 },
+  { id: 38, category: 'Untitled', url: img38 },
+  { id: 39, category: 'Untitled', url: img39 },
+  { id: 40, category: 'Reflection and Ocean', url: img40 },
+  { id: 41, category: 'For Friends', url: img41 },
+  // { id: 42, category: 'Glasses', url: img42 },
+  { id: 43, category: 'Glasses', url: img43 },
+  { id: 44, category: 'Glasses', url: img44 },
+  { id: 45, category: 'My Muse', url: img45 },
+  { id: 49, category: 'My Muse', url: img49 },
+  { id: 52, category: 'My Muse', url: img52 },
+  { id: 53, category: 'My Muse', url: img53 },
+  { id: 54, category: 'My Muse', url: img54 },
+  { id: 55, category: 'My Muse', url: img55 },
+  { id: 56, category: 'My Muse', url: img56 },
+  { id: 57, category: 'My Muse', url: img57 },
+  { id: 58, category: 'My Muse', url: img58 },
+  { id: 59, category: 'My Muse', url: img59 },
+  { id: 60, category: 'My Muse', url: img60 },
+  { id: 62, category: 'My Muse', url: img62 },
+  { id: 61, category: 'My Muse', url: img61 },
+  { id: 64, category: 'My Muse', url: img64 },
+  { id: 63, category: 'My Muse', url: img63 },
+  { id: 66, category: 'My Muse', url: img66 },
+  { id: 65, category: 'My Muse', url: img65 },
+  { id: 68, category: 'My Muse', url: img68 },
+  { id: 67, category: 'My Muse', url: img67 },
+  { id: 69, category: 'My Muse', url: img69 },
+  { id: 48, category: 'My Muse', url: img48 },
+  { id: 46, category: 'My Muse', url: img46 },
+  { id: 47, category: 'My Muse', url: img47 },
+  { id: 50, category: 'My Muse', url: img50 },
+  { id: 51, category: 'My Muse', url: img51 },
 ]
 
 export default Images
