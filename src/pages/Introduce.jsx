1ea89270140d53ec227c3fc5f2c21261c76eadf7
ella choy <egal21de@gmail.com>
2024-06-13 10:43:07 +0200

    Entfernt unnötigen Text

--- src/pages/Introduce.jsx
+++ src/pages/Introduce.jsx
@@ -12,20 +12,13 @@ export default function Introduce() {
           </div>
           <div className='intro-block'>
             <h2 className='h3'>Hello, i am Bobae Kim </h2>
-            <blockquote>
-              I love seeing things through other objects.
-              <br></br> 나는 사물을 투과해 보이는 것들을 사랑한다.
-            </blockquote>
+            <blockquote>I love seeing things through other objects.</blockquote>
             <p>
               Between time and seasons, between objects, between moments of
               love... everything has gaps. Even in emotions, gaps always exist.
               I love those gaps. Sometimes, various emotions are contained
               within those gaps—longing, loneliness, affection. I want to
               capture and express them.
-              <br></br>시간, 계절사이, 물건, 사랑의 사이,.. 모든 것에는 틈이
-              존재한다. 하물며 감정에도 틈이란 것은 늘 존재하고 있다. 그 틈을
-              나는 사랑한다. 때로는 그 틈에 여러 감정이 내포된다. 그리움,
-              외로움, 사랑스러움. 그것들을 나는 그려내고자 한다.
             </p>
           </div>
         </div>
